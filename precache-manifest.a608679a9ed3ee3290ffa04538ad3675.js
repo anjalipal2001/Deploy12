ba32b84c20b9e9b6b497814c6f785d147eb6168e
anjalipal2001 <ap7197339@gmail.com>
2021-04-11 17:55:58 +0530

    Updates

---
+++ precache-manifest.a608679a9ed3ee3290ffa04538ad3675.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea37b3d03f9480c3a6f6476ddb9c7064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a67118efd3a55a7ce29",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "b8eb48c2001675b28106",
+    "url": "/static/css/main.86a5c374.chunk.css"
+  },
+  {
+    "revision": "0a67118efd3a55a7ce29",
+    "url": "/static/js/2.6796a955.chunk.js"
+  },
+  {
+    "revision": "20377f3c2f2703e596dd85276960f64e",
+    "url": "/static/js/2.6796a955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8eb48c2001675b28106",
+    "url": "/static/js/main.c278355b.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  },
+  {
+    "revision": "a6827cf5a483c13359d03d74eeb3951e",
+    "url": "/static/media/1.a6827cf5.jpg"
+  },
+  {
+    "revision": "b35d99509a21157f84888a170ddfbfed",
+    "url": "/static/media/2.b35d9950.jpg"
+  },
+  {
+    "revision": "5989ebbfe5acdbbce7d80b77164a9b0c",
+    "url": "/static/media/3.5989ebbf.jpg"
+  },
+  {
+    "revision": "5abb223434e6ae5ebfb6091d2ffe2db6",
+    "url": "/static/media/4.5abb2234.jpg"
+  },
+  {
+    "revision": "414026f730ac98c9ae5761c0efe6efa1",
+    "url": "/static/media/5.414026f7.jpg"
+  },
+  {
+    "revision": "54f2288fa1db1b0569dd4d74aa200172",
+    "url": "/static/media/card.54f2288f.jpg"
+  },
+  {
+    "revision": "84799a34e5bbc050be6e0f284ad7b26f",
+    "url": "/static/media/emptyBookmarks.84799a34.svg"
+  },
+  {
+    "revision": "93ba78358a09a1026c6cd1322a084502",
+    "url": "/static/media/emptyCart.93ba7835.svg"
+  },
+  {
+    "revision": "159260d8a74b0cbad3439696b1da28f7",
+    "url": "/static/media/logo.159260d8.svg"
+  },
+  {
+    "revision": "b9dcccae818c5cbea8e792529ed61557",
+    "url": "/static/media/success.b9dcccae.svg"
+  }
+]);
